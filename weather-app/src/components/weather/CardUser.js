1da059e1a7198988166eae55285a0f7f812fb824
Nancy <nan.tap.24@gmail.com>
2021-05-09 22:11:36 -0500

    [FEAT]: Log out facebook and google

--- weather-app/src/components/weather/CardUser.js
+++ weather-app/src/components/weather/CardUser.js
@@ -2,21 +2,28 @@ import React, { useContext }  from 'react';
 import Box from '@material-ui/core/Box';
 import Avatar from '@material-ui/core/Avatar';
 import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+
 import { UserContext } from "../../providers/UserProvider";
+import {auth} from "../../firebase";
 
 const CardUser = () =>  {
 
     const user = useContext(UserContext);
-    const {displayName} = user;
+    const {photoURL,displayName} = user;
 
     return (
         <Box mt={1}>
             <Grid container direction="column" justify="center" alignItems="center">
                 <Grid item xs={12}>
-                    <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                    <Avatar alt="Remy Sharp" src={photoURL} />
                 </Grid>
                 <Grid item xs={12} style={{padding: "2% 0% 8% 0%"}}>
                     {displayName}
+                    <IconButton aria-label="exit"  size="small" style={{color: "white"}} onClick = {() => {auth.signOut()}}> 
+                        <ExitToAppIcon fontSize="inherit" />
+                    </IconButton>
                 </Grid>
             </Grid>
         </Box>
